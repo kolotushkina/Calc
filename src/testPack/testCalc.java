0a1ee9942b65ebf83901e9c8900936ec00e46b68
Raushan <35721857+kolotushkina@users.noreply.github.com>
2018-09-17 08:46:53 +0300

    upd JUnitTest

--- src/testPack/testCalc.java
+++ src/testPack/testCalc.java
@@ -2,10 +2,8 @@ package testPackMain;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
+
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import packMain.Calc;
 
